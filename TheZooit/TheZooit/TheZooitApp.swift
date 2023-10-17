2580aa639ffd445b849cad5380d1b60b99bdb66a
Yuliia Vanchytska <julia.van28@gmail.com>
2023-10-17 16:28:28 +0200

    Initial commit

---
+++ TheZooit/TheZooit/TheZooitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheZooitApp.swift
+//  TheZooit
+//
+//  Created by Yuliia on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TheZooitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
