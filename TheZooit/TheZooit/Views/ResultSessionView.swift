8c143f22504dd78e4fde39e0a338e5f1feaa6183
Gianmarco Cremisi <marcomega0064@gmail.com>
2023-10-23 18:06:45 +0200

    Create a session-result-view - and ad speechBubble_image

--- TheZooit/TheZooit/Views/ResultSessionView.swift
+++ TheZooit/TheZooit/Views/ResultSessionView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ResultSessionView: View {
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
         NavigationStack {
             ZStack {
@@ -59,58 +60,100 @@ struct ResultSessionView: View {
                                         .stroke(Color("customLightGreen"), lineWidth: 2.5))
                         }
                         
-                    }.padding(.horizontal, 20)
+                    }
+                    .padding(.horizontal, 20)
                     
-                    Spacer()
                     
                     Text("Horray!").bold().font(.system(size: 42))
                         .fontWidth(.expanded).foregroundStyle(Color ("customLightGreen"))
                         .padding(.bottom, 30)
                     //onlyrectangle
-                    Rectangle()
-                        .frame( width: 298,height: 436)
-                        .clipShape(RoundedRectangle(cornerRadius: 50))
-                        .foregroundStyle(Color("backgroundColor"))
-                        .overlay(RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color("customGreen"), lineWidth: 1.2)
-                        )
                     
-                        .padding(.bottom, 70)
-                    //Home button
-                    NavigationLink(destination: ContentView()) {
-                        
-                        HStack{
-                            ZStack{
-                                
-                                Text("Home").font(.system(size: 28))
-                                    .fontWeight(.bold)
-                                    .fontWidth(.expanded).foregroundStyle(Color ("customLightGreen"))
-                                
-                                Rectangle()
-                                    .frame( width: 298,height: 68)
-                                    .clipShape(RoundedRectangle(cornerRadius: 50))
-                                    .foregroundStyle(Color("backgorundColor"))
-                                    .overlay(RoundedRectangle(cornerRadius: 50)
-                                        .stroke(Color("customLightGreen"), lineWidth: 3)
-                                    )
-                                   
+                    ZStack(alignment: .bottom){
+                        Rectangle()
+                            .overlay { 
+                                VStack(alignment: .leading){
+                                    Text("You have studied for 10 minutes!").foregroundStyle(Color.white).font(.system(size: 24))
+                                        .fontWidth(.expanded).padding(.bottom, 20)
+                                    
+                                    Text("Your reward:")
+                                        .foregroundStyle(Color.white).font(.system(size: 24)).fontWeight(.medium)
+                                        .fontWidth(.expanded)
+                                    HStack{
+                                        Image(systemName:
+                                                "dollarsign.circle").foregroundStyle(Color.white)
+                                            .font(.system(size: 24)).fontWeight(.medium)
+                                            .fontWidth(.expanded)
+                                        Text("10 coins")
+                                            .foregroundStyle(Color.white).font(.system(size: 24)).fontWeight(.regular)
+                                            .fontWidth(.expanded)
+                                    }
+                                }
+                                .padding(.bottom, 180)
                             }
-                            .padding(.bottom, 20 )
+                            .frame( width: 298,height: 436)
+                            .clipShape(RoundedRectangle(cornerRadius: 50))
+                            .foregroundStyle(Color("backgroundColor"))
+                            .overlay(RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color("customGreen"), lineWidth: 1.2)).padding(.bottom, 45)
+                        
+                        HStack(alignment: .bottom, spacing: -35){
+                            
+                            Image("SpeachBubble")
+                                .overlay{
+                                    Text("Good job!").foregroundStyle(Color.black)
+                                        .font(.system(size: 24)).fontWeight(.bold)
+                                        .fontWidth(.expanded)
+                                        .padding(.bottom, 10)
+                                    
+                                }
+                                .aspectRatio(contentMode: .fit)
+                                .padding(.bottom, 200.0)
+                                .frame(width: 174.0, height: 141.0)
+                            
+                            
+                            
+                            Image("frog-happy")
+                                .resizable().aspectRatio(contentMode: .fit).padding().frame(width: 150, height: 175.0).rotationEffect(.degrees(7))
+                            
+                            
+                            
                         }
+                        .padding(.leading, 100)
                         
                     }
+                    
+                    
+                    .padding(.bottom, 30)
+                    //Home button
+                    Button(action: {
+                        dismiss()
+                    }) {
+                    label: do {
+                        Text("Home").font(.system(size: 28))
+                            .fontWeight(.bold)
+                            .fontWidth(.expanded)
+                            .foregroundColor(Color ("customLightGreen")).padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 56)
+                                    .stroke(Color ("customLightGreen"), lineWidth: 3).frame(width: 298,height: 68)
+                            )
+                    }}
+                    .padding(.bottom, 20 )
                 }
                 
-                
-                
-                
             }
-            
-            
         }
         
+        
+        
+        
     }
+    
+    
 }
+
+
 
 
 #Preview {
